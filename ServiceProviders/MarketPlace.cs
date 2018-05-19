a6ded01593c4ad4869688853950d7b77c6fe8fe7
Carla Bosco <carlabosco@gmail.com>
2018-05-19 15:11:00 -0700

    Initial commit

---
+++ ServiceProviders/MarketPlace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceProviders
+{
+    class MarketPlace
+    {
+        public string ItemName { get; set; }
+        public double Price { get; set; }
+        public string Location { get; set; }
+    }
+}
