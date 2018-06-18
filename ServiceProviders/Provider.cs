a850bda396228ccc140ad8c45589fc0fde6a4399
Carla Bosco <carlabosco@gmail.com>
2018-06-18 15:30:41 -0700

    Added collection to MarketPlace class

--- ServiceProviders/Provider.cs
+++ ServiceProviders/Provider.cs
@@ -20,25 +20,21 @@ namespace ServiceProviders
 
     class Provider
     {
-        private static int baseProviderNumber = 0;
-
         public string Name { get; set; }
         public ServiceType Service { get; set; }
         public string City { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public int ProviderNumber { get; private set; }
-
 
         //Need GUI to finish constructor. 
-        public Provider(string name, ServiceType service, string city, string email, string phoneNumber = "Email only")
-        {
-            ProviderNumber = ++baseProviderNumber;
-            Name = name;
-            Service = service;
-            City = city;
-            Email = email;
-            PhoneNumber = phoneNumber;
-        }
+        //public Provider(string name, ServiceType service, string city, string email, string phoneNumber = "Email only")
+        //{
+        //    ProviderNumber = ++baseProviderNumber;
+        //    Name = name;
+        //    Service = service;
+        //    City = city;
+        //    Email = email;
+        //    PhoneNumber = phoneNumber;
+        //}
     }
 }
